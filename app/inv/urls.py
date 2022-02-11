d46587e34a83f2c8987ddef2e630dbfffc649ebc
catuchowil <gapuentes@hotmail.com.ar>
2022-02-11 12:46:39 -0300

    Hatas la implementacion del DataTable - video 72

--- app/inv/urls.py
+++ app/inv/urls.py
@@ -1,9 +1,10 @@
 from unicodedata import name
 from django.urls import path
 
-from .views import CategoriaView, CategoriaNew
+from .views import CategoriaView, CategoriaNew, CategoriaEdit
 
 urlpatterns = [
     path('categorias/',CategoriaView.as_view(), name='categoria_list'),
-    path('categorias/new',CategoriaNew.as_view(),name='categoria_new')
+    path('categorias/new',CategoriaNew.as_view(),name='categoria_new'),
+    path('categorias/edit/<int:pk>',CategoriaEdit.as_view(),name='categoria_edit')
 ]
--- app/inv/views.py
+++ app/inv/views.py
@@ -28,3 +28,16 @@ class CategoriaNew(LoginRequiredMixin,generic.CreateView):
         form.instance.uc = self.request.user
         return super().form_valid(form)
 
+
+
+class CategoriaEdit(LoginRequiredMixin,generic.UpdateView):
+    model = Categoria
+    template_name = 'inv/categoria_form.html'
+    context_object_name = 'obj'
+    form_class = CategoriaForm
+    success_url = reverse_lazy('inv:categoria_list')
+    login_url = 'base:login'
+
+    def form_valid(self,form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
