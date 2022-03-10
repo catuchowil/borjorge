4552550b74a2d463c1a1dde5720cfbcc66164ecb
catuchowil <gapuentes@hotmail.com.ar>
2022-03-10 10:36:58 -0300

    CRUD unidad de medida

--- app/inv/forms.py
+++ app/inv/forms.py
@@ -1,7 +1,7 @@
 from dataclasses import field
 from tkinter import Widget
 from django import forms 
-from .models import Categoria,SubCategoria, Marca
+from .models import Categoria,SubCategoria, Marca, UnidadMedida
 
 class CategoriaForm(forms.ModelForm):
 
@@ -59,6 +59,23 @@ class SubCategoriaForm(forms.ModelForm):
 class MarcaForm(forms.ModelForm):
     class Meta:
         model=Marca
+        fields = ['descripcion','estado']
+        labels= {'descripcion': "Descripción de la Marca",
+                "estado":"Estado"}
+        widget={'descripcion': forms.TextInput()}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+
+
+
+class UMForm(forms.ModelForm):
+    class Meta:
+        model=UnidadMedida
         fields = ['descripcion','estado']
         labels= {'descripcion': "Descripción de la Marca",
                 "estado":"Estado"}
